91a04c982e2febcf49d998ded8b3fdcbd1376fd1
Leonardo Leite <leonardo.oliveira2f@gmail.com>
2022-11-08 17:36:17 -0300

    adicionando validacoes do nickName

--- sistema-login/Cadastro/Model/ValidacaoDeUsuarioParaCadastro.swift
+++ sistema-login/Cadastro/Model/ValidacaoDeUsuarioParaCadastro.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ValidacaoDeUsuarioParaCadastro: NSObject {
     
+    // MARK: - Atributos
     private let controladorDeErros: ControladorDeErros
     
+    // MARK: - Inicializador
     init(controladorDeErros: ControladorDeErros) {
         self.controladorDeErros = controladorDeErros
     }
     
+    // MARK: - Funcao principal
     public func usuarioPodeSerCadastrado(
         nickNameDoUsuario: String?,
         nomeCompletoDoUsuario: String?,
@@ -44,58 +47,88 @@ class ValidacaoDeUsuarioParaCadastro: NSObject {
         return usuarioPodeSerCadastrado
     }
     
+    // MARK: - Funcoes secundarias
     private func validacoesDoNickNameDoUsuario(_ nickName: String) -> Bool {
+        var isValid = true
+        
         let nickNameDoUsuarioEstaVazio = strlen(nickName) == 0
         if nickNameDoUsuarioEstaVazio {
-            self.controladorDeErros.adicionarErro(erro: .erro_usuario_vazio)
+            self.controladorDeErros.adicionarErro(erro: .erro_nick_de_usuario_vazio)
             
-            return false
+            isValid = false
         }
         
-        return true
+        let nickNameDoUsuarioTemMenosDe5Caracteres = strlen(nickName) < 5
+        if nickNameDoUsuarioTemMenosDe5Caracteres {
+            self.controladorDeErros.adicionarErro(erro: .erro_nick_de_usuario_tem_menos_de_5_caracteres)
+            
+            isValid = false
+        }
+        
+        let nickNameDoUsuarioNaoÉUmAlfaNumerico = !nickName.isAlphanumeric
+        if nickNameDoUsuarioNaoÉUmAlfaNumerico {
+            self.controladorDeErros.adicionarErro(erro: .erro_nick_de_usuario_nao_e_um_alfanumerico)
+        }
+        
+        return isValid
     }
     
     private func validacoesDoNomeCompletoDoUsuario(_ nomeCompleto: String) -> Bool {
+        var isValid = true
+        
         let nomeCompletoDoUsuarioEstaVazio = strlen(nomeCompleto) == 0
         if nomeCompletoDoUsuarioEstaVazio {
             self.controladorDeErros.adicionarErro(erro: .erro_nome_vazio)
             
-            return false
+            isValid = false
         }
         
-        return true
+        return isValid
     }
     
     private func validacoesDoEmailDoUsuario(_ email: String) -> Bool {
+        var isValid = true
+        
         let emailDoUsuarioEstaVazio = strlen(email) == 0
         if emailDoUsuarioEstaVazio {
             self.controladorDeErros.adicionarErro(erro: .erro_email_vazio)
             
-            return false
+            isValid = false
         }
         
-        return true
+        return isValid
     }
     
     private func validacoesDaSenhaDoUsuario(_ senha: String) -> Bool {
+        var isValid = true
+        
         let senhaDoUsuarioEstaVazia = strlen(senha) == 0
         if senhaDoUsuarioEstaVazia {
             self.controladorDeErros.adicionarErro(erro: .erro_senha_vazia)
             
-            return false
+            isValid = false
         }
         
-        return true
+        return isValid
     }
     
     private func validacoesDaRepeticaoDeSenhaDoUsuario(_ repeticaoDeSenha: String) -> Bool {
+        var isValid = true
+        
         let repeticaoDaSenhaDoUsuarioEstaVazia = strlen(repeticaoDeSenha) == 0
         if repeticaoDaSenhaDoUsuarioEstaVazia {
             self.controladorDeErros.adicionarErro(erro: .erro_repeticao_de_senha_vazio)
             
-            return false
+            isValid = false
         }
         
-        return true
+        return isValid
+    }
+}
+
+// MARK: - Extensoes
+extension String {
+    var isAlphanumeric: Bool {
+        !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
     }
 }
--- sistema-login/Classes/Erros.swift
+++ sistema-login/Classes/Erros.swift
@@ -8,9 +8,20 @@
 import Foundation
 
 enum Erros: String {
-    case erro_usuario_vazio = "O campo usuário não pode ser vazio!"
+    case erro_nick_de_usuario_vazio = "O campo usuário não pode ser vazio!"
+    case erro_nick_de_usuario_tem_menos_de_5_caracteres = "O apelido de usuário não pode ter menos de 5 caracteres!"
+    case erro_nick_de_usuario_nao_e_um_alfanumerico = "O apelido só pode conter letras (sem acentos) ou números!"
+    
     case erro_nome_vazio = "O campo nome não pode ser vazio!"
+    
+    
     case erro_email_vazio = "O campo e-mail não pode ser vazio!"
+    
+    
     case erro_senha_vazia = "O campo senha não pode ser vazio!"
+    
+    
     case erro_repeticao_de_senha_vazio = "O campo repetição da senha não pode ser vazio!"
+    
+    
 }

908a78325ee05386421f0a218e8be7bf02b55af5
Leonardo Oliveira <leonardo.oliveira2f@gmail.com>
2022-11-11 16:49:02 -0300

    criando a view da home

---
+++ sistema-login/Views/Home/HomeView.swift
@@ -0,0 +1,12 @@
+//
+//  HomeView.swift
+//  sistema-login
+//
+//  Created by Leonardo Leite on 11/11/22.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+}
---
+++ sistema-login/Views/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  sistema-login
+//
+//  Created by Leonardo Leite on 11/11/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .lightGray
+    }
+}

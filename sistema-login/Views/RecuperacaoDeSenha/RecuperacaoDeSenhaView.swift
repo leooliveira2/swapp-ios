ed9f023089dde7588208886b4f757875c624e8c5
Leonardo Oliveira <leonardo.oliveira2f@gmail.com>
2022-11-22 16:42:36 -0300

    adicionando as primeiras constraints da view

--- sistema-login/Views/RecuperacaoDeSenha/RecuperacaoDeSenhaView.swift
+++ sistema-login/Views/RecuperacaoDeSenha/RecuperacaoDeSenhaView.swift
@@ -27,12 +27,34 @@ class RecuperacaoDeSenhaView: UIView {
         return label
     }()
     
+    // MARK: - Inicializadores
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.configsConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Constraints
+    private func configsConstraints() -> Void {
+        self.addSubview(self.backgroundImageView)
+        self.addSubview(self.tituloRecuperaSenhaLabel)
+        
+        NSLayoutConstraint.activate([
+            self.backgroundImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            self.backgroundImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.backgroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            self.backgroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            self.tituloRecuperaSenhaLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.tituloRecuperaSenhaLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            
+        
+        
+        ])
     }
     
 }

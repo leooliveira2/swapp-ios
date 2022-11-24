25c88d2593a719914ac263826f0e2891a0914ae2
Leonardo Oliveira <leonardo.oliveira2f@gmail.com>
2022-11-24 17:23:30 -0300

    criando o componente nave button

--- sistema-login/Views/Home/Telas/Nave/NaveView.swift
+++ sistema-login/Views/Home/Telas/Nave/NaveView.swift
@@ -17,6 +17,21 @@ class NaveView: UIView {
         return imageView
     }()
     
+    private lazy var gerarNaveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Gerar Nave", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
+        button.titleLabel?.textAlignment = .center
+        button.setTitleColor(UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0), for: .normal)
+        button.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        button.clipsToBounds = true
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1.5
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
     // MARK: - Inicializadores
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,12 +45,18 @@ class NaveView: UIView {
     // MARK: - Configs constraints
     private func configsConstraints() -> Void {
         self.addSubview(self.backgroundImageView)
+        self.addSubview(self.gerarNaveButton)
+        
         NSLayoutConstraint.activate([
             self.backgroundImageView.topAnchor.constraint(equalTo: self.topAnchor),
             self.backgroundImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.backgroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             self.backgroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
         
+            self.gerarNaveButton.topAnchor.constraint(equalTo: self.centerYAnchor),
+            self.gerarNaveButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            self.gerarNaveButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            self.gerarNaveButton.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
 }

7d9cab218f2e44212b424b3ba4fd127d3b2bb6ed
Leonardo Oliveira <leonardo.oliveira2f@gmail.com>
2022-11-24 17:10:15 -0300

    criando componente botao gerar planeta

--- sistema-login/Views/Home/Telas/Planeta/PlanetaView.swift
+++ sistema-login/Views/Home/Telas/Planeta/PlanetaView.swift
@@ -17,6 +17,21 @@ class PlanetaView: UIView {
         return imageView
     }()
     
+    private lazy var gerarPlanetaButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Gerar Planeta", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 22)
+        button.titleLabel?.textAlignment = .center
+        button.setTitleColor(UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0), for: .normal)
+        button.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        button.clipsToBounds = true
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1.5
+        button.layer.cornerRadius = 20
+        return button
+    }()
+    
     // MARK: - Inicializadores
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,12 +45,18 @@ class PlanetaView: UIView {
     // MARK: - Configs constraints
     private func configsConstraints() -> Void {
         self.addSubview(self.backgroundImageView)
+        self.addSubview(self.gerarPlanetaButton)
         
         NSLayoutConstraint.activate([
             self.backgroundImageView.topAnchor.constraint(equalTo: self.topAnchor),
             self.backgroundImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             self.backgroundImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             self.backgroundImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            self.gerarPlanetaButton.topAnchor.constraint(equalTo: self.centerYAnchor),
+            self.gerarPlanetaButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            self.gerarPlanetaButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            self.gerarPlanetaButton.heightAnchor.constraint(equalToConstant: 50)
         
         ])
     }
